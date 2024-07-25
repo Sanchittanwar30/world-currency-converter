a078de053968552087d0c0ea818e19a0d0a1372c
Sanchittanwar30 <111452909+Sanchittanwar30@users.noreply.github.com>
2024-07-25 13:12:21 +0530

    app.js

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const base_url="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+const base_url="https://latest.currency-api.pages.dev/v1/currencies";
 
 const ddowns = document.querySelectorAll(".dropdown select");
+const btn= document.querySelector("form button");
+
+const fromCurr= document.querySelector(".from select");
+const toCurr=document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
 for(let select of ddowns){
     for(currcode in countryList){
         let newopt = document.createElement("option");
         newopt.innerText=currcode;
         newopt.value=currcode;
-        select.append(newopt);
+        
 
-        if(select.name ==="From" && currcode === "USD"){
+        if(select.name ==="from" && currcode === "USD"){
             newopt.selected = "selected";
         }
-        if(select.name ==="To" && currcode === "INR"){
+        else if(select.name ==="to" && currcode === "INR"){
             newopt.selected = "selected";
         }
+        select.append(newopt);
 
     }
     select.addEventListener("change",(evt)=>{
@@ -22,9 +28,39 @@ for(let select of ddowns){
     })
 }
 
-const flag =() =>{
-    
-
+const flag =(element) =>{
+let currcode=element.value;
+let cntcode= countryList[currcode];
+let newsrc= `https://flagsapi.com/${cntcode}/flat/64.png`;
+let img = element.parentElement.querySelector("img");
+img.src=newsrc;
 
 }
 
+const updaterate = async () =>{
+    let amount = document.querySelector(".amount input");
+    let amtval = amount.value;
+    if(amtval ==="" || amtval<1){
+        amtval=1;
+        amount.value="1";
+    }
+  
+   const url = `${base_url}/${fromCurr.value.toLowerCase()}.json`;
+   let response = await fetch(url);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let totAmt = rate*amtval;
+
+    msg.innerText = `${amtval} ${fromCurr.value} = ${totAmt} ${toCurr.value}`;
+
+}
+
+btn.addEventListener("click",  (evt)=>{
+    evt.preventDefault();
+    updaterate();
+
+})
+
+window.addEventListener("load", () => {
+    updaterate();
+  });
